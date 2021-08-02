0bf6af3f3aea441586d11d6ee0c9e2793ec9f456
web-dave <dave@webdave.de>
2021-08-02 21:55:35 +0200

    eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,49 +4,50 @@ import { tap } from 'rxjs/operators';
 
 interface Item {
   name: string;
-anzahl: number;
-preis: number;
-erledigt: boolean;
+  anzahl: number;
+  preis: number;
+  erledigt: boolean;
 }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   itemListBS$ = new BehaviorSubject<Item[]>([]);
-  itemList$ = this.itemListBS$.asObservable().pipe(tap(data => localStorage.setItem('itemList', JSON.stringify(data))))
+  itemList$ = this.itemListBS$
+    .asObservable()
+    .pipe(
+      tap((data) => localStorage.setItem('itemList', JSON.stringify(data)))
+    );
   title = 'shopping-cart';
   modalVisible = 'none';
-  items: Item[] = []
+  items: Item[] = [];
 
-
-  ngOnInit(){
-    this.items = JSON.parse(localStorage.getItem('itemList') || '[]')
-    this.itemListBS$.next(this.items)
-
+  ngOnInit() {
+    this.items = JSON.parse(localStorage.getItem('itemList') || '[]');
+    this.itemListBS$.next(this.items);
   }
-  deleteItem(i: number){
-    this.items.splice(i, 1)
-    this.itemListBS$.next(this.items)
+  deleteItem(i: number) {
+    this.items.splice(i, 1);
+
+    this.itemListBS$.next(this.items);
   }
 
-  addItem(name: string, anzahl: string, preis: string){
-    this.items.push(
-      {
-        name,
+  addItem(name: string, anzahl: string, preis: string) {
+    this.items.push({
+      name,
       anzahl: parseInt(anzahl, 10),
       preis: parseFloat(preis),
-      erledigt: false
-      }
-    )
+      erledigt: false,
+    });
     this.modalVisible = 'none';
-   this.itemListBS$.next(this.items)
+    this.itemListBS$.next(this.items);
   }
 
-  setErledigt(item: Item){
-    item.erledigt = !item.erledigt
-   this.itemListBS$.next(this.items)
+  setErledigt(item: Item) {
+    item.erledigt = !item.erledigt;
+    this.itemListBS$.next(this.items);
   }
 }
