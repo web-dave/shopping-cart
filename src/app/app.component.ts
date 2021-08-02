5e8931df8f1595c2b423a74dcad54acf4fec1a68
web-dave <dave@webdave.de>
2021-08-02 21:48:17 +0200

    first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+interface Item {
+  name: string;
+anzahl: number;
+preis: number;
+erledigt: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  itemListBS$ = new BehaviorSubject<Item[]>([]);
+  itemList$ = this.itemListBS$.asObservable().pipe(tap(data => localStorage.setItem('itemList', JSON.stringify(data))))
   title = 'shopping-cart';
+  modalVisible = 'none';
+  items: Item[] = []
+
+
+  ngOnInit(){
+    this.items = JSON.parse(localStorage.getItem('itemList') || '[]')
+    this.itemListBS$.next(this.items)
+
+  }
+  deleteItem(i: number){
+    this.items.splice(i, 1)
+    this.itemListBS$.next(this.items)
+  }
+
+  addItem(name: string, anzahl: string, preis: string){
+    this.items.push(
+      {
+        name,
+      anzahl: parseInt(anzahl, 10),
+      preis: parseFloat(preis),
+      erledigt: false
+      }
+    )
+    this.modalVisible = 'none';
+   this.itemListBS$.next(this.items)
+  }
+
+  setErledigt(item: Item){
+    item.erledigt = !item.erledigt
+   this.itemListBS$.next(this.items)
+  }
 }
